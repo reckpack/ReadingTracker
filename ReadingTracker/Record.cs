0b170ebf0036fe2779506d7d60870c04e27fa362
reckpack <sammy_ender@yahoo.com>
2018-12-13 00:32:44 -0500

    record class

---
+++ ReadingTracker/Record.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTracker
+{
+
+    class Record
+    {
+        // basics
+        int m_id;
+        string m_name;
+        string m_book_type;
+        int m_current_read;
+        int m_latest_released;
+        bool m_is_finished;
+
+        // details
+        string m_genre;
+        string m_publisher;
+        string m_category_id;
+        DateTime m_initial_release_date;
+        string[] m_writters;
+        string[] m_artists;
+
+        
+    }
+}
